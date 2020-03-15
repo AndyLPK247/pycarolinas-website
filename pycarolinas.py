8ef31d304581c43be9cbb9dc174e0252c57fd4ef
AndyLPK247 <AndyLPK247@gmail.com>
2020-03-15 15:31:36 -0400

    added page stubs

--- pycarolinas.py
+++ pycarolinas.py
@@ -21,3 +21,43 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
   return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
+@app.route('/code-of-conduct')
+def code_of_conduct():
+  return render_template('code-of-conduct.html')
+
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+
+@app.route('/covid19')
+def covid19():
+  return render_template('covid19.html')
+
+
+@app.route('/diversity')
+def diversity():
+  return render_template('diversity.html')
+
+
+@app.route('/speaking')
+def speaking():
+  return render_template('speaking.html')
+
+
+@app.route('/sponsors')
+def sponsors():
+  return render_template('sponsors.html')
+
+
+@app.route('/venue')
+def venue():
+  return render_template('venue.html')
